70bd37719a904edd563983c832369cc41f053bba
Deny Kurnia Hermawan <denykurnia040@gmail.com>
2024-06-14 22:43:58 +0700

    Membuat intent dari MainActivity ke Login dan Login Ke Mainactivity(back)

--- app/src/main/java/com/uti/lavender_sobat_organik/LoginActivity.java
+++ app/src/main/java/com/uti/lavender_sobat_organik/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.uti.lavender_sobat_organik;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        ImageView btnBack;
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnBack = findViewById(R.id.L_img1);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pindahactivity = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(pindahactivity);
+
+            }
         });
+
     }
 }
--- app/src/main/java/com/uti/lavender_sobat_organik/MainActivity.java
+++ app/src/main/java/com/uti/lavender_sobat_organik/MainActivity.java
@@ -1,6 +1,9 @@
 package com.uti.lavender_sobat_organik;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +13,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+        Button btnLoginListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
+        btnLoginListener = findViewById(R.id.btn_1);
+        btnLoginListener.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent pindahlogin = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(pindahlogin);
+            }
+        });
     }
 
 }
