62586a94d4a23f936f4fff3217a72dd4e5c454f9
Deny Kurnia Hermawan <denykurnia040@gmail.com>
2024-06-16 01:30:44 +0700

    Menambahkan mainactivity search dan favorite dan membuat intent dari beranda ke search

--- app/src/main/java/com/uti/lavender_sobat_organik/BerandaActivity.java
+++ app/src/main/java/com/uti/lavender_sobat_organik/BerandaActivity.java
@@ -1,6 +1,11 @@
 package com.uti.lavender_sobat_organik;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class BerandaActivity extends AppCompatActivity {
 
+    Button btnSearch;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_beranda);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+    btnSearch=findViewById(R.id.B_search);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent pindahsearch = new Intent(BerandaActivity.this, SearchActivity.class);
+                    startActivity(pindahsearch);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         });
+
+
+
     }
 }
