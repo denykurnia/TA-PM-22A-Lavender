d9212d262d921dce3f130cf50bb1852524606897
Yulian Samsir <YulianSamsir8@gmail.com>
2024-06-16 17:04:58 +0700

    Menambahkan class java dan luyout baru lalu membuat intent Icon shop dri BerandaActivity

--- app/src/main/java/com/uti/lavender_sobat_organik/BerandaActivity.java
+++ app/src/main/java/com/uti/lavender_sobat_organik/BerandaActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BerandaActivity extends AppCompatActivity {
 
